a7b99aac17069b676c3e8e55f4cbaabb6dbef1e4
Baldstrom <jadenbottemiller@gmail.com>
2019-06-09 13:31:46 -0700

    Working on SPI test

--- control/spiTest/spiTest.ino
+++ control/spiTest/spiTest.ino
@@ -3,27 +3,53 @@ char buff [50];
 volatile byte indx;
 volatile boolean process;
 
+int MODE = 0;
+int X = 0;
+int Y = 0;
+
 void setup (void) {
-   Serial.begin (115200);
+   Serial.begin(9600);
    pinMode(MISO, OUTPUT); // have to send on master in so it set as output
    SPCR |= _BV(SPE); // turn on SPI in slave mode
    indx = 0; // buffer empty
    process = false;
    SPI.attachInterrupt(); // turn on interrupt
 }
-ISR (SPI_STC_vect) // SPI interrupt routine { 
+
+
+ISR (SPI_STC_vect) {// SPI interrupt routine 
    byte c = SPDR; // read byte from SPI Data Register
    if (indx < sizeof buff) {
       buff [indx++] = c; // save data in the next index in the array buff
-      if (c == '\r') //check for the end of the word
-      process = true;
+      if (c == '\0') { //check for the end of the word
+        process = true;
+      }
+   }
+}
+
+void processSPI()
+{
+  if (process) {
+      process = false; //reset the process
+      Serial.println(buff); //print the array on serial monitor
+      char sw = buff[0];
+      if (sw == "S") 
+      {
+        
+      }
+      else if (sw == "X") 
+      {
+        
+      }
+      else if (sw == "Y")
+      {
+        
+      }
+      
+      indx = 0; //reset button to zero
    }
 }
 
 void loop (void) {
-   if (process) {
-      process = false; //reset the process
-      Serial.println (buff); //print the array on serial monitor
-      indx= 0; //reset button to zero
-   }
+   processSPI();
 }
